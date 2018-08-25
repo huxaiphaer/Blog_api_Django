33b7c8489560fd341ba6e0fae035513ac3d4cb1e
Idris Lutaaya <huxaiphaeridris@gmail.com>
2018-08-25 23:29:58 +0300

    update views

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+""""
+add render
+"""
+
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
@@ -6,11 +9,15 @@ from .serializers import PostSerializer
 # Create your views here.
 
 class PostList(generics.ListAPIView):
-    
+    """
+    post list
+    """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-
+    """
+    post detail
+    """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
